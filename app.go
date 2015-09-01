30829283031979d6f7b160fc2f9a9a2a578baeaf
Thar Htet <kotharthar@gmail.com>
2015-09-01 08:48:39 +0630

    Form template test.

--- app.go
+++ app.go
@@ -3,12 +3,36 @@ package main
 import (
 	"fmt"
 	"github.com/gorilla/mux"
+	"html/template"
 	"net/http"
 )
+
+const templ = `<h1>New Item</h1>
+{{.Aok}}
+<form action="items" method="post">
+    <input type="" id="name" value="" name="" />
+    <input type="Submit" class="button" value="" name="Create" />
+</form>
+`
+
+type Data struct {
+	Aok string
+}
 
 /* Get All Items */
 func getItems(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "All Items")
+}
+
+func newItem(w http.ResponseWriter, r *http.Request) {
+	data := Data{
+		Aok: "aok",
+	}
+
+	t := template.New("newItemForm")
+	//t, _ = t.ParseFiles(templ)
+	t, _ = t.ParseFiles("newItem.html")
+	t.Execute(w, data)
 }
 
 func createItem(w http.ResponseWriter, r *http.Request) {
@@ -37,6 +61,7 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/items", getItems).Methods("GET")
 	r.HandleFunc("/items", createItem).Methods("POST")
+	r.HandleFunc("/items/new", newItem).Methods("GET")
 	r.HandleFunc("/items/{item_id}", getItem).Methods("GET")
 	r.HandleFunc("/items/{item_id}", updateItem).Methods("PUT")
 	r.HandleFunc("/items/{item_id}", destroyItem).Methods("DELETE")
